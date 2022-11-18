a0e8d8b50447774ce9a5ad4167fb6a4152e6e943
svonidze <svonidze@mail.ru>
2022-11-18 21:30:43 +0300

    remove enum name extension since this was added in Dart 2.15

--- lib/src/enums/request_type.dart
+++ lib/src/enums/request_type.dart
@@ -1,13 +1,7 @@
-import 'package:translator/src/extensions/enum_extensions.dart';
-
 enum RequestType {
   translation,
   alternativeTranslation,
   synonym,
   definition,
   example,
-}
-
-extension RequestTypeExtensions on RequestType {
-  String get name => describeEnum(this)!;
 }
--- lib/src/extensions/enum_extensions.dart
+++
@@ -1,7 +0,0 @@
-String? describeEnum(Object? object) {
-  if (object != null) {
-    final nameParts = object.toString().split(".");
-    if (nameParts.isNotEmpty && nameParts.length == 2) return nameParts[1];
-  }
-  return null;
-}
